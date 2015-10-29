32024c962f77afb8b1f3348661e16bb4dc19b0b5
ChenJie <chenjie@chenjiedeimac.local>
2015-10-29 09:48:50 +0800

    Initial Commit

---
+++ CycleScrollViewDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CycleScrollViewDemo
+//
+//  Created by ChenJie on 15/10/29.
+//  Copyright © 2015年 ChenJie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CycleScrollViewDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  CycleScrollViewDemo
+//
+//  Created by ChenJie on 15/10/29.
+//  Copyright © 2015年 ChenJie. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
